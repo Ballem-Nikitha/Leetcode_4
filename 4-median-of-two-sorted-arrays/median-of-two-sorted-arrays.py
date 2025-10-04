56ca055e9897199687e1909274f91657038a5ce8
Ballem Nikitha <bnikitha9618@gmail.com>
2025-10-04 19:03:53 +0530

    Time: 3 ms (62.65%) | Memory: 12.7 MB (51.15%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -1,13 +1,30 @@
-class Solution(object):
+class Solution:
     def findMedianSortedArrays(self, nums1, nums2):
-        nums=sorted(nums1+nums2)
-        n=len(nums)
-        if n%2!=0:
-            mid=n//2
-            return float(nums[mid])
-        else:
-            mid1=n//2-1
-            mid2=n//2
-            res=(nums[mid1]+nums[mid2])/2.0
-            return res
-        
+        if len(nums1) > len(nums2):
+            nums1, nums2 = nums2, nums1
+
+        m, n = len(nums1), len(nums2)
+        total = m + n
+        half = (total + 1) // 2
+        left, right = 0, m
+
+        while left <= right:
+            i = (left + right) // 2
+            j = half - i
+
+            left1 = nums1[i - 1] if i > 0 else float('-inf')
+            right1 = nums1[i] if i < m else float('inf')
+            left2 = nums2[j - 1] if j > 0 else float('-inf')
+            right2 = nums2[j] if j < n else float('inf')
+
+            if left1 <= right2 and left2 <= right1:
+                if total % 2 == 0:
+                    return (max(left1, left2) + min(right1, right2)) / 2.0
+                else:
+                    return max(left1, left2)
+            elif left1 > right2:
+                right = i - 1
+            else:
+                left = i + 1
+
+        return 0.0
